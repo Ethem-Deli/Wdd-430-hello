2111706caadd04ed6d9594a7ee4d2610dc11dfe8
Ethem-Deli <e.deli5595@gmail.com>
2025-11-15 21:25:46 +0300

    Chapter 15 Adding Metadata

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+import type { Metadata } from 'next/types';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export default function Page() {
+  return <p>Customers Page</p>;
+}
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,9 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
- 
+ import type { Metadata } from 'next/types';
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
